fe7da3eff7336bb7f0f5a9364df84901662c7868
Bojun Sun <sunbojun@hotmail.com>
2015-12-02 00:24:51 -0500

    fix bug

--- FrontEnd/app/controller/homeCtrl.js
+++ FrontEnd/app/controller/homeCtrl.js
@@ -1,6 +1,7 @@
 blackboardApp.controller('HomeController', function ($scope, userService) {
 	
 	$scope.status = 'login';
+	$scope.isStudent;
     $scope.setStatus = function(status){
         $scope.status = status;
     };
@@ -9,7 +10,12 @@ blackboardApp.controller('HomeController', function ($scope, userService) {
     };
 
     $scope.student = new Student;
+
     $scope.doSignup = function (){
+    	if ($scope.isStudent == "false") {
+	    	$scope.student.isStudent = false;
+	    }
+	    
 		console.log($scope.student);
 
 		userService.signup($scope.student).then(function(res) {
