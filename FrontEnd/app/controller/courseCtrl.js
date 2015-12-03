0620c5a98c2cc89f729ec0b09cb0466fa8c98622
Bojun Sun <sunbojun@hotmail.com>
2015-12-02 21:10:55 -0500

    add school

--- FrontEnd/app/controller/courseCtrl.js
+++ FrontEnd/app/controller/courseCtrl.js
@@ -1,7 +1,9 @@
 blackboardApp.controller('CourseController', function ($scope, userService, $state) {
 	$scope.test = "page";
 	console.log(userService.getStudent());
-
+	$scope.curStudent = userService.getStudent();
+	$scope.curSchool = GlobalSchool[$scope.curStudent.schoolId - 1];
+	console.log($scope.curSchool);
 	$scope.changeProfile = function() {
 		console.log("change");
 		$state.go('signup');
